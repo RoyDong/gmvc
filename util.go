3ba9b99df7cbc9c33880a95c4a340fef9a664088
Roy Dong <i@roydong.com>
2016-06-21 11:26:08 +0800

    add round

---
+++ util.go
@@ -0,0 +1,17 @@
+package gmvc
+
+import "math"
+
+func Round(f float64, n int) float64 {
+    base := math.Pow10(n)
+
+    f = f * base
+
+    if f < 0 {
+        f = math.Ceil(f - 0.5) / base
+    } else {
+        f = math.Floor(f + 0.5) / base
+    }
+
+    return f
+}
